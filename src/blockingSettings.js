bf2a25b84852825c6221b6f25931d23ad0575664
Mikhail Fedorov <464d41@gmail.com>
2021-03-24 18:32:57 -0700

    refactor blocking settings

--- src/blockingSettings.js
+++ src/blockingSettings.js
@@ -6,54 +6,17 @@ import Form from 'react-bootstrap/Form';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 export default class BlockingSettings extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toggleAlarm = this.toggleAlarm.bind(this);
-        this.toggleBlock = this.toggleBlock.bind(this);
-        this.toggleLearn = this.toggleLearn.bind(this);
-        this.addViolation = this.addViolation.bind(this);
-        this.delViolation = this.delViolation.bind(this);
-    }
-    toggleAlarm(e) {
-        const policy = this.props.policy;
-        policy.policy.blockingSettings.violations.violations[e.target.id].alarm = !policy.policy.blockingSettings.violations.violations[e.target.id].alarm
-        this.props.onChange(policy);
-    }
-    toggleBlock(e) {
-        const policy = this.props.policy;
-        policy.policy.blockingSettings.violations.violations[e.target.id].block = !policy.policy.blockingSettings.violations.violations[e.target.id].block
-        this.props.onChange(policy);
-    }
-    toggleLearn(e) {
-        const policy = this.props.policy;
-        policy.policy.blockingSettings.violations.violations[e.target.id].learn = !policy.policy.blockingSettings.violations.violations[e.target.id].learn
-        this.props.onChange(policy);
-    }
-    addViolation(e) {
-        const policy = this.props.policy;
-        const violation = policy.getAllViolations().find(v => v.name === e.target.id);
-        policy.policy.blockingSettings.violations.add(violation);
-        this.props.onChange(policy);
-    }
-    delViolation(e) {
-        const policy = this.props.policy;
-        policy.policy.blockingSettings.violations.del(e.target.id)
-        this.props.onChange(policy);
-    }
     render() {
         return (
             <div>
                 <h2>Blocking Settings</h2>
                 <ViolationsList
-                    violations={this.props.policy.getAllViolations()}
-                    onClick={this.addViolation}>
+                    policy={this.props.policy}
+                    onChange={this.props.onChange}>
                 </ViolationsList>
                 <Violations
                     policy={this.props.policy}
-                    toggleAlarm={this.toggleAlarm}
-                    toggleBlock={this.toggleBlock}
-                    toggleLearn={this.toggleLearn}
-                    delViolation={this.delViolation}>
+                    onChange={this.props.onChange}>
                 </Violations>
             </div>
         );
@@ -61,13 +24,19 @@ export default class BlockingSettings extends React.Component {
 }
 
 class ViolationsList extends React.Component {
+    addViolation(e) {
+        const policy = this.props.policy;
+        const violation = policy.getAllViolations().find(v => v.name === e.target.id);
+        policy.blockingSettings.violations.push(violation);
+        this.props.onChange(policy);
+    }
     render() {
         return (
             <Dropdown className="mb-2">
                 <Dropdown.Toggle id="dropdown-basic">Violations</Dropdown.Toggle>
                 <Dropdown.Menu>
-                    {this.props.violations.map((violation) => (
-                        <Dropdown.Item key={violation.name} id={violation.name} onClick={this.props.onClick}>{violation.title}</Dropdown.Item>
+                    {this.props.policy.getAllViolations().map((violation) => (
+                        <Dropdown.Item key={violation.name} id={violation.name} onClick={e => this.addViolation(e)}>{violation.title}</Dropdown.Item>
                     ))}
                 </Dropdown.Menu>
             </Dropdown>
@@ -75,6 +44,31 @@ class ViolationsList extends React.Component {
     }
 }
 class Violations extends React.Component {
+    toggleAlarm(e) {
+        const policy = this.props.policy;
+        policy.blockingSettings.violations[e.target.id].alarm = !policy.blockingSettings.violations[e.target.id].alarm
+        this.props.onChange(policy);
+    }
+    toggleBlock(e) {
+        const policy = this.props.policy;
+        policy.blockingSettings.violations[e.target.id].block = !policy.blockingSettings.violations[e.target.id].block
+        this.props.onChange(policy);
+    }
+    toggleLearn(e) {
+        const policy = this.props.policy;
+        policy.blockingSettings.violations[e.target.id].learn = !policy.blockingSettings.violations[e.target.id].learn
+        this.props.onChange(policy);
+    }
+    delViolation(e) {
+        const policy = this.props.policy;
+        policy.blockingSettings.violations.splice(e.target.id)
+        this.props.onChange(policy);
+    }
+    delAllViolations(e) {
+        const policy = this.props.policy;
+        policy.blockingSettings.violations = [];
+        this.props.onChange(policy);
+    }
     render() {
         return (
             <div>
@@ -85,11 +79,15 @@ class Violations extends React.Component {
                             <th >Alarm</th>
                             <th>Block</th>
                             <th>Learn</th>
-                            <th></th>
+                            <th>
+                                <Button size="sm" onClick={e => this.delAllViolations(e)}>
+                                    Remove
+                                </Button>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.policy.policy.blockingSettings.violations.violations?.map((violation, index) => (
+                        {this.props.policy.blockingSettings.violations?.map((violation, index) => (
                             <tr key={violation.name}>
                                 <td className="text-left">
                                     {violation.title}
@@ -98,7 +96,7 @@ class Violations extends React.Component {
                                     <Form.Check
                                         id={index.toString()}
                                         checked={violation.alarm || false}
-                                        onChange={e => this.props.toggleAlarm(e)}>
+                                        onChange={e => this.toggleAlarm(e)}>
                                     </Form.Check>
                                 </td>
                                 <td>
@@ -106,7 +104,7 @@ class Violations extends React.Component {
                                         type="checkbox"
                                         id={index.toString()}
                                         checked={violation.block || false}
-                                        onChange={e => this.props.toggleBlock(e)}>
+                                        onChange={e => this.toggleBlock(e)}>
                                     </Form.Check>
                                 </td>
                                 <td>
@@ -114,13 +112,13 @@ class Violations extends React.Component {
                                         type="checkbox"
                                         id={index.toString()}
                                         checked={violation.learn || false}
-                                        onChange={e => this.props.toggleLearn(e)}>
+                                        onChange={e => this.toggleLearn(e)}>
                                     </Form.Check>
                                 </td>
                                 <td>
                                     <Button size="sm"
                                         id={index.toString()}
-                                        onClick={e => this.props.delViolation(e)}>
+                                        onClick={e => this.delViolation(e)}>
                                         Remove
                                         </Button>
                                 </td>
