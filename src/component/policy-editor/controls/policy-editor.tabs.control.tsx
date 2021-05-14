10154b3535330defe3f768bb856985e1cc657f65
Github Automatic Deploy <info@yoctocode.com>
2021-05-14 20:35:56 +0300

    scaffolding: policy editor tabs

---
+++ src/component/policy-editor/controls/policy-editor.tabs.control.tsx
@@ -0,0 +1,43 @@
+import withStyles from "@material-ui/core/styles/withStyles";
+import { createStyles, Theme } from "@material-ui/core";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import * as React from "react";
+
+export type EditorTabProps = {
+  label: JSX.Element;
+};
+
+export const EditorTabsControl = withStyles((theme) =>
+  createStyles({
+    root: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    indicator: {
+      backgroundColor: theme.palette.primary.main,
+    },
+  })
+)(Tabs);
+
+export const EditorTabControl = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      textTransform: "none",
+      minWidth: 72,
+      fontWeight: theme.typography.fontWeightRegular,
+      marginRight: theme.spacing(4),
+      "&:hover": {
+        color: theme.palette.primary.main,
+        opacity: 1,
+      },
+      "&$selected": {
+        color: theme.palette.primary.main,
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+      "&:focus": {
+        color: theme.palette.primary.main,
+      },
+    },
+    selected: {},
+  })
+)((props: EditorTabProps) => <Tab disableRipple {...props} />);
--- src/component/policy-editor/policy-editor.component.tsx
+++ src/component/policy-editor/policy-editor.component.tsx
@@ -2,10 +2,29 @@ import * as React from "react";
 import { Typography } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 
+import { useState } from "react";
+import { TabsTree } from "./controls/policy-editor.tabs.tree";
+import {
+  EditorTabControl,
+  EditorTabsControl,
+} from "./controls/policy-editor.tabs.control";
+
 export const PolicyEditorComponent: React.VoidFunctionComponent = () => {
+  const [currentTab, setCurrentTab] = useState<number>(0);
+
   return (
-    <Box>
-      <Typography>Policy editor</Typography>
-    </Box>
+    <div>
+      <EditorTabsControl
+        value={currentTab}
+        onChange={(_, tab) => setCurrentTab(tab)}
+      >
+        {TabsTree.map(({ label, id }) => (
+          <EditorTabControl label={label} key={id} />
+        ))}
+      </EditorTabsControl>
+      <Box>
+        <Typography>Policy editor {currentTab}</Typography>
+      </Box>
+    </div>
   );
 };
