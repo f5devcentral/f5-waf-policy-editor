22a92ab17ac48f0032d178e203f02b7920c561f1
Github Automatic Deploy <info@yoctocode.com>
2021-05-21 14:46:38 +0300

    styling fields table

--- src/component/policy-editor/controls/grid.table-value.control.tsx
+++ src/component/policy-editor/controls/grid.table-value.control.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { FieldResolverVisitor } from "../../../store/policy-editor/visitor/interface/field-resolver.visitor";
-import { TableContainer } from "@material-ui/core";
+import {
+  createStyles,
+  Tab,
+  TableContainer,
+  withStyles,
+} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
@@ -10,6 +15,29 @@ import TableBody from "@material-ui/core/TableBody";
 import { GridFieldValue } from "./grid.field-value.control";
 import Typography from "@material-ui/core/Typography";
 import Checkbox from "@material-ui/core/Checkbox";
+import { DeleteForeverRounded } from "@material-ui/icons";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+
+const StyledTableCell = withStyles((theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.background.default,
+      color: theme.palette.primary.contrastText,
+    },
+  })
+)(TableCell);
+
+const StyledTableRow = withStyles((theme) =>
+  createStyles({
+    root: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  })
+)(TableRow);
 
 export type GridTableValueProps = {
   titles: string[];
@@ -18,44 +46,77 @@ export type GridTableValueProps = {
 
 export const GridTableValueControl: React.FunctionComponent<GridTableValueProps> =
   ({ titles, visitors }) => {
-    const createValueControl: (
+    const createValueCell: (
       item: GridFieldValue,
       onChange: any
     ) => JSX.Element = (item: GridFieldValue, onChange) => {
       switch (typeof item.value) {
         case "boolean": {
           return (
-            <Checkbox
-              checked={item.value}
-              onChange={(e) => onChange(e.target.checked)}
-            />
+            <TableCell padding={"checkbox"} size="small" align="center">
+              <Checkbox
+                size="small"
+                checked={item.value}
+                color="primary"
+                onChange={(e) => onChange(e.target.checked)}
+              />
+            </TableCell>
           );
         }
         default: {
-          return <Typography>{item.value}</Typography>;
+          return (
+            <TableCell size="small">
+              <Typography variant={"caption"}>{item.value}</Typography>
+            </TableCell>
+          );
         }
       }
     };
 
     return (
-      <TableContainer component={Paper}>
+      <TableContainer component={Box}>
         <Table>
           <TableHead>
             <TableRow>
+              <StyledTableCell padding={"checkbox"} size="small" align="center">
+                <Checkbox size="small" color="primary" />
+              </StyledTableCell>
+
               {titles.map((x, index) => (
-                <TableCell key={index}>{x}</TableCell>
+                <StyledTableCell key={index}>
+                  <Typography color="primary">{x}</Typography>
+                </StyledTableCell>
               ))}
+              <StyledTableCell>
+                <Typography>
+                  <Button color="primary" variant="contained">
+                    Remove
+                  </Button>
+                </Typography>
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {visitors.map((v, vIndex) => (
-              <TableRow key={vIndex}>
-                {v.getRows().map((item, itemIndex) => (
-                  <TableCell key={itemIndex}>
-                    {createValueControl(item, item.onChange)}
-                  </TableCell>
-                ))}
-              </TableRow>
+              <StyledTableRow key={vIndex}>
+                <StyledTableCell
+                  padding={"checkbox"}
+                  size="small"
+                  align="center"
+                >
+                  <Checkbox size="small" color="primary" />
+                </StyledTableCell>
+                {v
+                  .getRows()
+                  .map((item, itemIndex) =>
+                    createValueCell(item, item.onChange)
+                  )}
+                <TableCell size="small" align="center">
+                  <IconButton size="small">
+                    <DeleteForeverRounded />
+                  </IconButton>
+                </TableCell>
+              </StyledTableRow>
             ))}
           </TableBody>
         </Table>
