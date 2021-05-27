0f20a4387b098e883ae52666276e5a688e376188
Github Automatic Deploy <info@yoctocode.com>
2021-05-27 18:45:12 +0300

    added support for ref query parameter

--- src/store/policy-editor/policy-editor.reducer.ts
+++ src/store/policy-editor/policy-editor.reducer.ts
@@ -11,7 +11,6 @@ import {
 } from "../action-types";
 import { policyEditorPageSetHandler } from "./handler/policyeditor-page-set.handler";
 import { policyEditorJsonTextSetHandler } from "./handler/policyeditor-jsontext-set.handler";
-import { defaultGeneralSettings } from "../../model/policy-editor.defaults.model";
 import { policyEditorJsonVisitHandler } from "./handler/policyeditor-json-visit.handler";
 
 export function policyEditorStateInit(): PolicyEditorState {
@@ -19,8 +18,8 @@ export function policyEditorStateInit(): PolicyEditorState {
     currentTab: 0,
     jsonParseError: false,
     currentPage: PolicyEditorPageEnum.GeneralSettings,
-    jsonCurrentPolicy: defaultGeneralSettings(),
-    strCurrentPolicy: JSON.stringify(defaultGeneralSettings(), null, 2),
+    jsonCurrentPolicy: {},
+    strCurrentPolicy: "",
   };
 }
 
