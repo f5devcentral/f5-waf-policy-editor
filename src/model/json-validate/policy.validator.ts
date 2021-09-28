b7393d7a99f5110c7357b6838b61c02b1ab192a5
Github Automatic Deploy <info@yoctocode.com>
2021-09-28 11:09:18 +0300

    add custom schema validation as well as custom error messages

--- src/model/json-validate/policy.validator.ts
+++ src/model/json-validate/policy.validator.ts
@@ -1,5 +1,8 @@
 import { Validator } from "jsonschema";
 import * as wafSchema from "../policy-schema/policy-schema.json";
+import * as customPolicy from "../policy-schema/custom-updates.policy-schema.json";
+
+import { merge as _merge } from "lodash";
 
 export type PolicyValidationError = {
   message: string;
@@ -10,18 +13,31 @@ export type PolicyValidationError = {
 export class PolicyValidator {
   validate(policy: any): PolicyValidationError[] {
     try {
-      const validator = new Validator();
+      const schema = _merge(
+        customPolicy.properties.policy,
+        wafSchema.properties.policy
+      );
 
-      const errors = validator.validate(
-        policy,
-        wafSchema.properties.policy as any,
-        {
-          nestedErrors: true,
-        }
-      ).errors;
+      const validator = new Validator();
+      validator.addSchema(customPolicy.properties.policy as any);
+      const errors = validator.validate(policy, schema as any, {
+        nestedErrors: true,
+      }).errors;
+
+      console.log(errors);
 
       return errors.map((x) => {
-        if (x.name === "required") {
+        const customMessage = (x.schema as any).message
+          ? (x.schema as any).message[x.name]
+          : undefined;
+
+        if (customMessage) {
+          return {
+            message: customMessage,
+            path: x.path,
+            property: x.property,
+          };
+        } else if (x.name === "required") {
           return {
             message: x.message,
             path: x.path,
