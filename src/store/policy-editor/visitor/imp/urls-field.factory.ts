9020a4479a8c54c73dc1991197f61c0ea9a5da88
Github Automatic Deploy <info@yoctocode.com>
2021-10-18 22:24:19 +0300

    leave only required fields

--- src/store/policy-editor/visitor/imp/urls-field.factory.ts
+++ src/store/policy-editor/visitor/imp/urls-field.factory.ts
@@ -6,24 +6,4 @@ export class UrlsFieldFactory extends VisitorFactoryBase<URLElement> {
   constructor(protected dispatch: PolicyEditorDispatch, protected json: any) {
     super("policy.urls", dispatch, json);
   }
-  // create(defaultFunc, url): void {
-  //   this.dispatch(
-  //     policyEditorJsonVisit((currentJson) => {
-  //       const path = "policy.urls";
-  //       let urls = _get(currentJson, path);
-  //       if (!urls) {
-  //         _set(currentJson, path, [] as any);
-  //         urls = _get(currentJson, path);
-  //       }
-  //
-  //       const schemaService = new PolicySchemaService();
-  //       const val = schemaService.shrinkToRequired(
-  //         defaultUrls(urls.length, url),
-  //         "urls[]"
-  //       );
-  //
-  //       urls.push(val);
-  //     })
-  //   );
-  // }
 }
