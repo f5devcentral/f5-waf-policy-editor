b30021fd6ccda661ae004a23d5b1ce4b1a335eb5
Github Automatic Deploy <info@yoctocode.com>
2022-01-13 12:59:10 +0200

    1. Remove full policy switch. 2. Add supported evasions, htttp-protocols

--- src/component/policy-convert/policy-convert.sidebar.component.tsx
+++ src/component/policy-convert/policy-convert.sidebar.component.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { Switch } from "@mui/material";
-import { policyEditorShowDefaultPolicySet } from "../../store/policy-editor/policy-editor.actions";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import {
-  usePolicyEditorDispatch,
-  usePolicyEditorState,
-} from "../../store/policy-editor/policy-editor.hooks";
 
 export const PolicyConvertSidebarComponent: React.VoidFunctionComponent =
   () => {
-    const { showDefaultPolicy } = usePolicyEditorState();
-    const dispatch = usePolicyEditorDispatch();
+    // const { showDefaultPolicy } = usePolicyEditorState();
+    // const dispatch = usePolicyEditorDispatch();
 
     return (
       <React.Fragment>
@@ -21,18 +14,18 @@ export const PolicyConvertSidebarComponent: React.VoidFunctionComponent =
             paddingTop: "0px",
           }}
         >
-          <FormControlLabel
-            labelPlacement="end"
-            label="View full policy"
-            control={
-              <Switch
-                checked={showDefaultPolicy}
-                onChange={(e) => {
-                  dispatch(policyEditorShowDefaultPolicySet(e.target.checked));
-                }}
-              />
-            }
-          />
+          {/*<FormControlLabel*/}
+          {/*  labelPlacement="end"*/}
+          {/*  label="View full policy"*/}
+          {/*  control={*/}
+          {/*    <Switch*/}
+          {/*      checked={showDefaultPolicy}*/}
+          {/*      onChange={(e) => {*/}
+          {/*        dispatch(policyEditorShowDefaultPolicySet(e.target.checked));*/}
+          {/*      }}*/}
+          {/*    />*/}
+          {/*  }*/}
+          {/*/>*/}
         </div>
       </React.Fragment>
     );
--- src/converter/strategy/imp-nap/http-protocols.parse-strategy.ts
+++ src/converter/strategy/imp-nap/http-protocols.parse-strategy.ts
@@ -1,11 +1,46 @@
 import { ParseStrategyBase } from "../parse-strategy.base";
 import { KeyParsingResultEnum } from "../../model/key-parsing-result.enum";
 import { StrategyLogItemModel } from "../../model/strategy-log-item.model";
+import { HTTPProtocol } from "../../../model/policy-schema/policy.definitions";
+
+const supportedHttpProtocols: string[] = [
+  "Multiple host headers",
+  "Header name with no header value",
+  "Null in request",
+  "Retrieving data. Wait a few seconds and try to cut or copy again.",
+];
 
 export class HttpProtocolsParseStrategy extends ParseStrategyBase {
   parse(policyObj: any, fullPath: string): void {
+    let anyNotSupportedFlag = false;
+    policyObj.forEach((x: HTTPProtocol) => {
+      if (supportedHttpProtocols.includes(x.description ?? "")) {
+        this.context.strategyLog.add(
+          new StrategyLogItemModel(
+            fullPath,
+            KeyParsingResultEnum.success,
+            x.description
+          )
+        );
+      } else {
+        anyNotSupportedFlag = true;
+        this.context.strategyLog.add(
+          new StrategyLogItemModel(
+            fullPath,
+            KeyParsingResultEnum.notSupported,
+            x.description
+          )
+        );
+      }
+    });
+
     this.context.strategyLog.add(
-      new StrategyLogItemModel(fullPath, KeyParsingResultEnum.partially)
+      new StrategyLogItemModel(
+        fullPath,
+        anyNotSupportedFlag
+          ? KeyParsingResultEnum.partially
+          : KeyParsingResultEnum.success
+      )
     );
   }
 }
