e2b9492d6971645fc19d51d4ca2f5f77f4f86076
Github Automatic Deploy <info@yoctocode.com>
2022-02-09 19:57:52 +0200

    add sensitive parameters as parameters parse

--- src/converter/strategy/imp-nap/parameters.parse-strategy.ts
+++ src/converter/strategy/imp-nap/parameters.parse-strategy.ts
@@ -1,11 +1,82 @@
 import { ParseStrategyBase } from "../parse-strategy.base";
 import { KeyParsingResultEnum } from "../../model/key-parsing-result.enum";
 import { StrategyLogItemModel } from "../../model/strategy-log-item.model";
+import {
+  ParameterElement,
+  ParameterLocation,
+} from "../../../model/policy-schema/policy.definitions";
 
 export class ParametersParseStrategy extends ParseStrategyBase {
   parse(policyObj: any, fullPath: string) {
+    if (!this.context.athenaFirewallDto.custom_anonymization) {
+      this.context.athenaFirewallDto.custom_anonymization = [];
+    }
+    let anyNotSensitiveFound = false;
+
+    const addCookie = (name: string) => {
+      if (!this.context.athenaFirewallDto.custom_anonymization) return;
+      this.context.athenaFirewallDto.custom_anonymization.push({
+        cookie: {
+          cookie_name: name,
+        },
+      });
+    };
+    const addHeader = (name: string) => {
+      if (!this.context.athenaFirewallDto.custom_anonymization) return;
+
+      this.context.athenaFirewallDto.custom_anonymization.push({
+        http_header: {
+          header_name: name,
+        },
+      });
+    };
+    const addQuery = (name: string) => {
+      if (!this.context.athenaFirewallDto.custom_anonymization) return;
+
+      this.context.athenaFirewallDto.custom_anonymization.push({
+        query_parameter: {
+          query_param_name: name,
+        },
+      });
+    };
+
+    policyObj.forEach((p: ParameterElement) => {
+      switch (true) {
+        case p.sensitiveParameter &&
+          p.parameterLocation === ParameterLocation.Any: {
+          addQuery(p.name);
+          addHeader(p.name);
+          addCookie(p.name);
+          break;
+        }
+        case p.sensitiveParameter &&
+          p.parameterLocation === ParameterLocation.Query: {
+          addQuery(p.name);
+          break;
+        }
+        case p.sensitiveParameter &&
+          (p.parameterLocation === ParameterLocation.Cookie || p.isCookie): {
+          addQuery(p.name);
+          break;
+        }
+        case p.sensitiveParameter &&
+          (p.parameterLocation === ParameterLocation.Header || p.isHeader): {
+          addHeader(p.name);
+          break;
+        }
+        default:
+          anyNotSensitiveFound = true;
+          break;
+      }
+    });
+
     this.context.strategyLog.add(
-      new StrategyLogItemModel(fullPath, KeyParsingResultEnum.success)
+      new StrategyLogItemModel(
+        fullPath,
+        anyNotSensitiveFound
+          ? KeyParsingResultEnum.partially
+          : KeyParsingResultEnum.success
+      )
     );
 
     return Promise.resolve();
