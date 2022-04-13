a81458e51405154d59e4181d9f68ec93e13977cd
Serge Levin <levin@yoctocode.com>
2022-04-13 16:39:21 +0100

    1. Add support for allowed and disallowed equal to false. 2. Add wildcard to the end for filetypes

--- src/converter/strategy/imp-nap/filetypes.parse-strategy.ts
+++ src/converter/strategy/imp-nap/filetypes.parse-strategy.ts
@@ -4,10 +4,11 @@ import { StrategyLogItemModel } from "../../model/strategy-log-item.model";
 import { Filetype } from "../../../model/policy-schema/policy.definitions";
 import { AthenaServicePolicyModel } from "../../model/athena-service-policy.model";
 import { transparentBlockUtil } from "./transparent-block.util";
+import { AthenaAction } from "../../model/athena-common.model";
 
 export class FiletypesParseStrategy extends ParseStrategyBase {
   parse(policyObj: Filetype[], fullPath: string) {
-    if (!this.context.athenaServicePolicy["filetypes"])
+    if (!this.context.athenaServicePolicy["filetypes"]) {
       this.context.athenaServicePolicy["filetypes"] = {
         metadata: {
           name: "filetypes",
@@ -20,36 +21,71 @@ export class FiletypesParseStrategy extends ParseStrategyBase {
           rule_list: {
             rules: [],
           },
-
         },
       } as AthenaServicePolicyModel;
+    }
 
-    policyObj.sort((a: Filetype, b: Filetype) => {
-      if (a.wildcardOrder !== undefined && b.wildcardOrder !== undefined) {
-        return a.wildcardOrder - b.wildcardOrder;
-      }
+    let hasWildcard = false;
+    let wildcardBlocking: AthenaAction = AthenaAction.ALLOW;
 
-      return 0;
-    }).forEach((x: Filetype) => {
-      if (this.context.athenaServicePolicy["filetypes"].spec.rule_list?.rules) {
-        this.context.athenaServicePolicy["filetypes"].spec.rule_list.rules.push(
-          {
-            metadata: {
-              name: x.name === "*" ? "any" : (x.name as string).toLowerCase(),
-            },
-            spec: {
-              action: transparentBlockUtil(x, !!this.context.athenaFirewallDto.blocking),
-              path: {
-                suffix_values: [x.name === "*" ? "any" : x.name],
+    policyObj
+      .sort((a: Filetype, b: Filetype) => {
+        if (a.wildcardOrder !== undefined && b.wildcardOrder !== undefined) {
+          return a.wildcardOrder - b.wildcardOrder;
+        }
+
+        return 0;
+      })
+      .forEach((x: Filetype) => {
+        if (
+          this.context.athenaServicePolicy["filetypes"].spec.rule_list?.rules
+        ) {
+          if (x.name === "*") {
+            hasWildcard = true;
+            wildcardBlocking = transparentBlockUtil(
+              x,
+              !!this.context.athenaFirewallDto.blocking
+            );
+          } else {
+            this.context.athenaServicePolicy[
+              "filetypes"
+            ].spec.rule_list.rules.push({
+              metadata: {
+                name: (x.name as string).toLowerCase(),
               },
-              waf_action: {
-                none: {}
+              spec: {
+                action: transparentBlockUtil(
+                  x,
+                  !!this.context.athenaFirewallDto.blocking
+                ),
+                path: {
+                  suffix_values: [x.name],
+                },
+                waf_action: {
+                  none: {},
+                },
               },
-            },
+            });
           }
-        );
-      }
-    });
+        }
+      });
+
+    if (
+      hasWildcard &&
+      this.context.athenaServicePolicy["filetypes"].spec.rule_list?.rules
+    ) {
+      this.context.athenaServicePolicy["filetypes"].spec.rule_list.rules.push({
+        metadata: {
+          name: "any",
+        },
+        spec: {
+          action: wildcardBlocking,
+          waf_action: {
+            none: {},
+          },
+        },
+      });
+    }
 
     this.context.strategyLog.add(
       new StrategyLogItemModel(fullPath, KeyParsingResultEnum.success)
--- src/converter/strategy/imp-nap/transparent-block.util.ts
+++ src/converter/strategy/imp-nap/transparent-block.util.ts
@@ -3,8 +3,8 @@ import { AthenaAction } from "../../model/athena-common.model";
 export function transparentBlockUtil(obj: any, isBlockingEnforcementMode: boolean) {
     if (!isBlockingEnforcementMode) return AthenaAction.ALLOW
 
-    if (obj.allowed) return AthenaAction.ALLOW;
-    if (obj.disallowed) return AthenaAction.DENY;
+    if ((obj.allowed) || (obj.disallowed !== undefined && obj.disallowed === false)) return AthenaAction.ALLOW;
+    if (obj.disallowed || (obj.allowed !== undefined && obj.allowed === false)) return AthenaAction.DENY;
 
     return AthenaAction.ALLOW;
 }
